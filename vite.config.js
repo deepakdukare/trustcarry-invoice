962b9648be79edc090822a2de598ba9305bc0be7
deepakdukare <deepakdukare2020@gmail.com>
2026-03-28 12:32:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000, // Silences the large chunk warning for jsPDF
+    chunkSizeWarningLimit: 2000
   }
 })
